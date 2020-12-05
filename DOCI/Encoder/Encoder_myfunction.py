cc563822f52e1db1d0e97501d014e1bf66efe0cc
jamjamjam <50306919+feynfeyn888@users.noreply.github.com>
2020-12-05 16:52:24 +0900

    Update Encoder_myfunction.py

--- DOCI/Encoder/Encoder_myfunction.py
+++ DOCI/Encoder/Encoder_myfunction.py
@@ -1,5 +1,6 @@
 #coding: utf-8 -*-
 
+import numpy as np
 #myfunc for encoder
 
 class MyClass:
@@ -9,7 +10,7 @@ class MyClass:
         binary = format(input, 'b')
         binary_array = [int(i) for i in binary]
         binary_array = [0]*(int(digit)-len(binary_array)) + binary_array
-        
+
         return binary_array
 
 
@@ -30,44 +31,44 @@ class MyClass:
 
     #a,b cahange into x,y--------------------------------------------
     def input(self, query, binary_array, bit_length):
-        
+
         a = query
         b = binary_array
 
-        #Query aの長さ
+        #bit_length = 16
         length = bit_length
-    
+
         #aと同じ長さの行列を作成する。ここではそれぞれ1×6行列
         a_ = np.zeros(length,dtype=np.int)
         b_ = np.zeros(length,dtype=np.int)
         x = np.zeros(length,dtype=np.int)
         y = np.zeros(length,dtype=np.int)
-        
+
         #a_に変換.1と0を反転させる
         for i in range(length):
             if a[i]==0:
                 a_[i]=1
             elif a[i]==1:
                 a_[i]=0    
-                
+
         #b_に変換
         for i in range(length):
             if b[i]==0:
                 b_[i]=1
             elif a[i]==1:
                 b_[i]=0 
-                
+
         #x生成 (a*b)
         #np.logical_and(a,b):a and b と同じ。and演算。
         c = np.logical_and(a,b)
-        
+
         #and演算の結果が1だったら1を返す
         for i in range(length):
             if c[i]==True:
                 x[i]=1
             elif c[i]==False:
                 x[i]=0
-                
+
         #y生成 (a_*b)
         c = np.logical_and(a_,b)
         for i in range(length):
@@ -75,15 +76,17 @@ class MyClass:
                 y[i]=1
             elif c[i]==False:
                 y[i]=0
-                
+
         #reshape([])
         #引数の使い方がよくわからん
         x = np.reshape(x,(1,length))
-        y = np.reshape(y,(1,length)) 
-        
+        y = np.reshape(y,(1,length))
+        x = x[0].tolist()
+        y = y[0].tolist()
+
         return x,y
 
-   def spatial_encode(self, input_x, input_y, bit_length, intensity, LED_array_length):
+    def spatial_encode(self, input_x, input_y, bit_length, intensity, LED_array_length):
 
         # intensity means "Intensity of light"
 
@@ -99,11 +102,11 @@ class MyClass:
             #左上
             if input_x[i]==0 and input_y[i]==0:
                 l[2*i + index*8][0] = intensity
-                 
+
             #右上
             if input_x[i]==0 and input_y[i]==1:
                 l[2*i + index*8+1][0] = intensity
-            
+
             #左下
             if input_x[i]==1 and input_y[i]==0:
                 l[2*i + index*8+8][0] = intensity
@@ -111,13 +114,13 @@ class MyClass:
             #右下
             if input_x[i]==1 and input_y[i]==1:
                 l[2*i + index*8+9][0] = intensity
-                
+
             #次の2*2の符号化画像アレイに移る
-        
+
         #reshape & tolist()x = np.reshape(x,(1,length))
         l = np.reshape(l,(1,LED_array_length)) 
         l = l[0].tolist()
- 
+
         return l
 
     def set_array(self, R, G, B, LED_array_length):
@@ -128,4 +131,4 @@ class MyClass:
             image_array.append([R[i],G[i],B[i]])
 
         return image_array
-     
+
